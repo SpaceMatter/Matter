cba4885ba20fe05bdf24d3269295d0c320792b01
SpaceMatter <62486763+SpaceMatter@users.noreply.github.com>
2020-03-22 22:24:41 +0100

    Update Javascript Script.js

--- Javascript Script.js
+++ Javascript Script.js
@@ -1,5 +1,20 @@
+var MatterAssemblerCost = 20
+var MatterAssemblers = 0;
 var Matter = 20;
 function Matterclick(number) {
   Matter = Matter + number;
   document.getElementById("MatterCount").innerHTML = Matter;
 };
+function buyMatterAssembler() {
+if (Matter >= MatterAssemblerCost) {
+  MatterAssemblers = MatterAssemblers + 1
+  Matter = Matter - MatterAssemblerCost
+  MatterAssemblerCost = MatterAssemblerCost * 2
+  document.getElementById('MatterAssemblerCost').innerHTML = MatterAssemblerCost
+  document.getElementById('MatterAssemblers').innerHTML = MatterAssemblers
+  document.getElementById("MatterCount").innerHTML = Matter
+}
+}
+window.setInterval(function(){
+Matterclick(MatterAssemblers)
+}, 100);
