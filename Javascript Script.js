0293abe7a2da8f5dd22f3f8a774d5eb9b6de7503
SpaceMatter <62486763+SpaceMatter@users.noreply.github.com>
2020-03-23 20:47:02 +0100

    Update Javascript Script.js

--- Javascript Script.js
+++ Javascript Script.js
@@ -1,20 +1,36 @@
+var Matter = 20
+var MatterProducentCost = 1000
+var MatterProducents = 0
+var MatterAssemblers = 0
 var MatterAssemblerCost = 20
-var MatterAssemblers = 0;
-var Matter = 20;
+function Tick() {
+document.getElementById('MatterAssemblerCost').innerHTML = MatterAssemblerCost
+document.getElementById('MatterAssemblers').innerHTML = MatterAssemblers
+document.getElementById('MatterProducents').innerHTML = MatterProducents
+document.getElementById('MatterProducentCost').innerHTML = MatterProducentCost
+document.getElementById("MatterCount").innerHTML = Matter
+}
 function Matterclick(number) {
   Matter = Matter + number;
-  document.getElementById("MatterCount").innerHTML = Matter;
 };
 function buyMatterAssembler() {
 if (Matter >= MatterAssemblerCost) {
   MatterAssemblers = MatterAssemblers + 1
   Matter = Matter - MatterAssemblerCost
   MatterAssemblerCost = MatterAssemblerCost * 2
-  document.getElementById('MatterAssemblerCost').innerHTML = MatterAssemblerCost
-  document.getElementById('MatterAssemblers').innerHTML = MatterAssemblers
-  document.getElementById("MatterCount").innerHTML = Matter
 }
+}
+function buyMatterProducent() {
+  if (Matter >= MatterProducentCost) {
+    MatterProducents = MatterProducents + 1
+    Matter = Matter - MatterProducentCost
+    MatterProducentCost = MatterProducentCost * 10
+  }
+
 }
 window.setInterval(function(){
 Matterclick(MatterAssemblers)
+MatterAssemblers = MatterAssemblers + MatterProducents
+Tick()
 }, 100);
+
